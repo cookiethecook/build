e56c478a3db94c0be5a66adc6f3ef9b3e76c4a76
Cookie, the Cook <cookie.the.cook@icloud.com>
2022-10-20 15:10:29 +0400

    commit

--- _next/static/1oPphPSh5Ta3ko7OjSvr2/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fbounties","\u002Fbounties\u002F[slug]","\u002Fmercenaries","\u002Fmercenaries\u002F[slug]","\u002Ftreasures","\u002Ftreasures\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
